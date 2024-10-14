d8e39136c8ebac9083e25c72e7edeecb0b313667
Nikhil Achalkar <142605075+nikhilachalkar@users.noreply.github.com>
2024-10-14 08:33:29 +0530

    Update TransactionsTable.js

--- src/components/TransactionsTable.js
+++ src/components/TransactionsTable.js
@@ -67,10 +67,10 @@ const TransactionsTable = ({ selectedMonth }) => {
       </table>
 
       <div className="pagination">
-        <button onClick={handlePrevPage} disabled={page === 1}>
+        <button  className = "button" onClick={handlePrevPage} disabled={page === 1}>
           Previous
         </button>
-        <button onClick={handleNextPage}>Next</button>
+        <button onClick={handleNextPage} className = "button">Next</button>
       </div>
     </div>
   );
