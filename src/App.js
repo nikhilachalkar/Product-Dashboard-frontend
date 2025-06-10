1ff8f6c8bf07604f1591e686de69e8e0374f842b
Nikhil Achalkar <142605075+nikhilachalkar@users.noreply.github.com>
2025-06-10 15:22:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import TransactionsTable from './components/TransactionsTable';
 import TransactionsStatistics from './components/TransactionsStatistics';
 import TransactionsBarChart from './components/TransactionsBarChart';
-import './App.css'; // Custom CSS for styling
+import './App.css';
 
 const App = () => {
   const [selectedMonth, setSelectedMonth] = useState(13); 
